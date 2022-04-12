30fdd3f9abf3529a43f10485e1b95a49805d8eaf
Alexei <alexei.prudnikov@gmail.com>
2022-04-12 21:05:25 +0300

    Add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,16 @@
+﻿void ReverseNumber(int number)
+{
+    if (number == 0) return;
+    int div = number / 10;
+    int mod = number % 10;
+    Console.Write(mod);
+    ReverseNumber(div);
+}
+
+Console.Clear();
+Console.WriteLine("==========Задача № 1==========");
+Console.WriteLine("Дано число n. Получите число, записанное в обратном порядке.");
+int number = 14235;
+Console.Write(number + " -> ");
+ReverseNumber(number);
+Console.WriteLine();
